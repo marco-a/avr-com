a75fa17f9889a31dd33ab5928666aa00b03bef96
Marco Agnoli <marco.agnoli@te.com>
2019-11-26 16:53:09 +0100

    Updated arduino code.

--- arduino/main.cpp
+++ arduino/main.cpp
@@ -1,43 +1,36 @@
 // Configuration
-#define DATA_PIN 6
-#define SYNC_PIN 5
+#define COM_DATA_PIN 6
+#define COM_SYNC_PIN 5
 
-#define SYNC_PULSE 100  /* in microseconds */
-#define DATA_TIME 1000  /* in microseconds */
+#define COM_SYNC_LENGTH 100   /* in microseconds */
+#define COM_DATA_LENGTH 1000  /* in microseconds */
 
 #define BAUDRATE 460800
 
 void setup() {
 	Serial.begin(BAUDRATE);
-	pinMode(DATA_PIN, INPUT);
-	pinMode(SYNC_PIN, INPUT);
+	pinMode(COM_DATA_PIN, INPUT);
+	pinMode(COM_SYNC_PIN, INPUT);
 }
 
 // used for edge detection
 bool previousSync = false;
-// not really neccessary..
-bool values[16];
 
 void loop() {
-	bool sync = digitalRead(SYNC_PIN);
+	bool sync = digitalRead(COM_SYNC_PIN);
 
 	// positive edge means transmission start
 	if (sync && !previousSync) {
 		// wait for sync pulse
 		// to go low + half of data transmission time
-		delayMicroseconds((SYNC_PULSE) + (DATA_TIME / 2));
+		delayMicroseconds((COM_SYNC_LENGTH) + (COM_DATA_LENGTH / 2));
 
 		for (uint8_t i = 0; i < 16; ++i) {
-			bool value = digitalRead(DATA_PIN);
+			bool value = digitalRead(COM_DATA_PIN);
 
-			values[i] = value;
+			Serial.print(value ? 1 : 0);
 
-			delayMicroseconds(DATA_TIME);
-		}
-
-		// print to serial
-		for (uint8_t i = 0; i < 16; ++i) {
-			Serial.print(values[i] ? 1 : 0);
+			delayMicroseconds(COM_DATA_LENGTH);
 		}
 
 		Serial.println("");
