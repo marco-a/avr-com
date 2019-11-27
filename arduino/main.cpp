6c82a6fc91a20c957ce2518533b9c83582ce74c4
Marco Agnoli <marco.agnoli@te.com>
2019-11-27 07:57:41 +0100

    Updated arduino code.

--- arduino/main.cpp
+++ arduino/main.cpp
@@ -1,40 +1,19 @@
-// Configuration
-#define COM_DATA_PIN 6
-#define COM_SYNC_PIN 5
+#include <SoftwareSerial.h>
 
-#define COM_SYNC_LENGTH 100   /* in microseconds */
-#define COM_DATA_LENGTH 1000  /* in microseconds */
+#define COM_PIN 13
+#define COM_BAUDRATE 1200
 
-#define BAUDRATE 460800
+SoftwareSerial mySerial(COM_PIN, 3); // RX, TX
 
 void setup() {
-	Serial.begin(BAUDRATE);
-	pinMode(COM_DATA_PIN, INPUT);
-	pinMode(COM_SYNC_PIN, INPUT);
+	Serial.begin(9600);
+	while (!Serial);
+	mySerial.begin(COM_BAUDRATE);
 }
-
-// used for edge detection
-bool previousSync = false;
 
 void loop() {
-	bool sync = digitalRead(COM_SYNC_PIN);
-
-	// positive edge means transmission start
-	if (sync && !previousSync) {
-		// wait for sync pulse
-		// to go low + half of data transmission time
-		delayMicroseconds((COM_SYNC_LENGTH) + (COM_DATA_LENGTH / 2));
-
-		for (uint8_t i = 0; i < 16; ++i) {
-			bool value = digitalRead(COM_DATA_PIN);
-
-			Serial.print(value ? 1 : 0);
-
-			delayMicroseconds(COM_DATA_LENGTH);
-		}
-
-		Serial.println("");
+	if (mySerial.available()) {
+		Serial.write(mySerial.read());
 	}
-
-	previousSync = sync;
 }
+
